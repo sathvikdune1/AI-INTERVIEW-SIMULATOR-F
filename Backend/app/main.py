3928335c7d26cab21b6e1e79d47d3c63fd332505
SathvikDune <22p31a4279@acet.ac.in>
2026-04-22 21:08:03 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -19,6 +19,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "https://ai-interview-simulator-xi.vercel.app"
     ],  # 🔥 allow all (for now)
     allow_credentials=True,
