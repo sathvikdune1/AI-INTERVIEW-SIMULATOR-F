7a9ac9d86313c1d0df1256f75e316cefb3be6edb
SathvikDune <22p31a4279@acet.ac.in>
2026-04-22 20:39:15 +0530

    Update API base URL for authentication

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL:"http://127.0.0.1:8000/api/auth"
+ baseURL:"http://54.175.234.64:8000"
 });
 
 export const registerUser = (data)=>{
